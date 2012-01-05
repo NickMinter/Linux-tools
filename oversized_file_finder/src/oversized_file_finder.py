20734454a289652e1b1ac947411fb6923a672ae0
nick <nick@nick-laptop.localdomain>
2012-01-06 10:30:19 +1100

    Adding oversized file finder project

---
+++ oversized_file_finder/src/oversized_file_finder.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="nick"
+__date__ ="$Jan 6, 2012 10:29:00 AM$"
+
+if __name__ == "__main__":
+    print "Hello World"
